abf4519bdcf4becb9a872bf7997f98bd5747d69c
KavyanshKhaitan2 <kavyanshkhaitan11@gmail.com>
2026-01-14 11:02:00 +0530

    Added registration support

--- common/urls.py
+++ common/urls.py
@@ -1,6 +1,8 @@
 from django.urls import include, path
-from .views import MyLoginView
+from .views import MyLoginView, MyLogoutView, RegisterView
 
 urlpatterns = [
+    path('register/', RegisterView.as_view(), name='register'),
     path('login/', MyLoginView.as_view(), name='login'),
+    path('logout/', MyLogoutView.as_view(), name='logout')
 ]
--- common/views.py
+++ common/views.py
@@ -1,7 +1,35 @@
-from django.shortcuts import render
-from django.contrib.auth.views import LoginView
+from django.shortcuts import render, redirect
+from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.messages import success, error, info
+from django.views import View
+from django.contrib.auth import get_user_model, login
+
+User = get_user_model()
 
 # Create your views here.
 
 class MyLoginView(LoginView):
     template_name = "login.html"
+
+class MyLogoutView(LogoutView):
+    def post(self, request, *args, **kwargs):
+        super().post(request, *args, **kwargs)
+        success(request, "Logged out")
+        return redirect("login")
+
+class RegisterView(View):
+    def get(self, request, *args, **kwargs):
+        if self.request.user.is_authenticated:
+            info(request, "You are already logged in.")
+            return redirect('dashboard')
+        return render(request, 'register.html')
+    def post(self, request, *args, **kwargs):
+        provided_username = request.POST['username']
+        provided_password = request.POST['password']
+        if User.objects.filter(username=provided_username).count():
+            error(request, "Different user with the same username already exists!")
+            return redirect(self.request.path_info)
+        user = User.objects.create_user(username=provided_username, password=provided_password)
+        login(request, user)
+        success(request, "Registered account successfully!")
+        return redirect('dashboard')
